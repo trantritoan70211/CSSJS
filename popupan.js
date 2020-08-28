e74a79b7c2f902684707cdcb8827074107587160
trantritoan70211 <55016781+trantritoan70211@users.noreply.github.com>
2020-08-28 19:41:01 +0700

    Update popupan.js

--- popupan.js
+++ popupan.js
@@ -1,8 +1,8 @@
 
-	var url_popup = 'https://sanhungthinh.net/can-ho/can-ho-ascent-garden-homes-quan-7';
+	var url_popup = 'https://tintucbds24h.info/can-ho-anderson-park-binh-duong';
 
       //<![CDATA[
-	  // var url_ppp = 'https://sanhungthinh.net/can-ho/can-ho-ascent-garden-homes-quan-7';
+	  // var url_ppp = 'https://tintucbds24h.info/can-ho-anderson-park-binh-duong';
       function addEvent(obj, eventName, func){
         if (obj.attachEvent)
         {
